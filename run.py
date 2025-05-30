f855caaa1f6bd784b5a38c2a3e9ee369e85469cb
mairima <aissatou.mairi@gmail.com>
2025-05-30 08:42:38 +0200

    Add print category result

--- run.py
+++ run.py
@@ -82,3 +82,4 @@ def run_bmi_quiz():
     category = ("Underweight" if bmi < 18.5 else
                 "Normal" if bmi < 25 else
                 "Overweight" if bmi < 30 else "Obese")    
+    print(f"\n{user_data['name']}, your BMI is {bmi} ({category})")
