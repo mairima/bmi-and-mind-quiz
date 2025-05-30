ea09fcc58f4e4864ebd1e50f88f62695df20b73c
mairima <aissatou.mairi@gmail.com>
2025-05-30 09:06:12 +0200

    Add choice option input between 1-3.strip()

--- run.py
+++ run.py
@@ -156,5 +156,6 @@ def run_bmi_quiz():
         print("1. View Rules")
         print("2. Take BMI Quiz")
         print("3. Take Mind Quiz")
+        choice = input("Choose an option (1-3): ").strip()
      
     
